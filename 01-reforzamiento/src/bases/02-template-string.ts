dc32f8027d6ad0d0f98aa03272f5c3393f88f15f
lmsf-101 <lmsf_academia_24@outlook.com>
2026-01-19 12:10:18 -0600

    14. Template string

---
+++ 01-reforzamiento/src/bases/02-template-string.ts
@@ -0,0 +1,10 @@
+const firstName = "Mike";
+const lastName = "O'Neil";
+
+// Usa backticks (``) para usar "template strings" que permite la concatenación de variables mediante el uso de ${}
+const fullName = `${firstName} ${lastName}`;
+
+console.log(fullName);
+
+
+
