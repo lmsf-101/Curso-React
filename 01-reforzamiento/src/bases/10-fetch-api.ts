59e2434ac98824295257c06d56866f07a853238b
lmsf-101 <lmsf_academia_24@outlook.com>
2026-01-28 12:01:01 -0600

    29. Agregado interfaces y optimizacion de soliticudes GIPHY

--- 01-reforzamiento/src/bases/10-fetch-api.ts
+++ 01-reforzamiento/src/bases/10-fetch-api.ts
@@ -1,3 +1,7 @@
+// Importación de tipo GiphyRandomResponse para incluir tipado a nuestra
+// respuesta HTTP de GIPHY:
+import type { GiphyRandomResponse } from "../bases/data/giphy.response"
+
 const API_KEY = import.meta.env.VITE_GIPHY_API_KEY
 
 // Fetch API : Administracion de solicitudes HTTP moderna de JS
@@ -5,26 +9,18 @@ const solicitud = fetch(
   `https://api.giphy.com/v1/gifs/random?api_key=${API_KEY}&tag=&rating=g`
 )
 
-// Forma 1: Convertir la respuesta en JSON, para después demostrar la data de
-  // GIPHY
-
-/*
-solicitud.then((res) => {
+// Creación de función para dividir responsabilidades (buena practica :) )
+const createImageInsideDOM = (url: string) => {
   
-  res.json().then(data => console.log(data));
+  const image = document.createElement('img');
+  image.src = url;
 
-}).catch(err => console.error(err))
-*/
+  document.body.append(image);
+}
 
-// Forma 2 más compacta
 solicitud.then((res) => res.json())
-.then((data) => {
-    const imgUrl = data.data.images.original.url;
-    console.log(imgUrl);
-
-    const imagen = document.createElement('img');
-    imagen.src = imgUrl;
-
-    document.body.append(imagen);
+.then(({data}: GiphyRandomResponse) => {
+    const imgUrl = data.images.original.url;
+    createImageInsideDOM(imgUrl);
 })
 .catch(err => console.error(err));
