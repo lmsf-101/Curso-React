064d75a094938cfddc45ef784c60cb6e5366e3db
lmsf-101 <lmsf_academia_24@outlook.com>
2026-01-29 14:21:35 -0600

    39. Ver impresion de variables en React

--- 02-first-steps/src/MyAwesomeApp.tsx
+++ 02-first-steps/src/MyAwesomeApp.tsx
@@ -1,9 +1,41 @@
-export function MyAwesomeApp() {
 
+/* - TIP: Si haces uso de variables constantes o fijas 
+    Es más recomendable que lo coloques fuera de las funciones /
+    componentes
+
+    Esto con el fin de evitar que React cargue las variables de forma
+    innecesaria:
+*/
+
+const firstName = "Miguel";
+const lastName = "Dante";
+
+const comidas = ["Manzana", "Bisquet", "Peras", "Aguacate"];
+
+const objeto = {
+  "nombre": "objeto",
+  tipo: "objeto"
+};
+
+export function MyAwesomeApp() {
   return (
     <>
-      <h1>Miguel</h1>
-      <h3>Dante</h3>
+      {/* - TIP: Insertar variables en React
+            Utilizan llaves {} para introducir variables en la aplicación */}
+      <h1> {firstName} </h1>
+      <h3> {lastName} </h3>
+      
+      {/* - TIP: Uso de sintaxis JS 
+      Las llaves nos permite ejecutar expresiones de JS
+      Por ejemplo podemos utilizar metodos de arreglos como join: */}
+      <p> {comidas.join(" - ")} </p>
+      
+      {/* - OJO : No podemos imprimir un objeto con su nombre tal cual
+      - ERROR : Objects are not valid as a React child */}
+      {/* <p> {objeto}</p> */}
+
+      {/* Existe una forma de imprimir el objeto: Usando JSON.stringify: */}
+      <p> {JSON.stringify(objeto)} </p>
     </>
   )
 
