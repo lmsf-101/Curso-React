fa5cc6cdc8bd3af5615cbffcdc30ee0d217d3d1b
lmsf-101 <lmsf_academia_24@outlook.com>
2026-01-28 19:53:29 -0600

    34. Mi primer proyecto React

---
+++ 02-first-steps/src/App.tsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    // Fragmentos : Agrupa elementos (como un contenedor)
+    <>
+      <h1>Hola Mundo</h1>
+      <p>Hola, lm</p>
+    </>
+  )
+}
+
+export default App
