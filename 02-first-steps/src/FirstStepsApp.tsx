6568c338ec9e89889afe365ef829c4b04ef27727
lmsf-101 <lmsf_academia_24@outlook.com>
2026-02-03 16:11:09 -0600

    43. Creación de componentes a partir de un listado / arreglo

--- 02-first-steps/src/FirstStepsApp.tsx
+++ 02-first-steps/src/FirstStepsApp.tsx
@@ -1,14 +1,26 @@
 import { ItemCounter } from "./shopping-cart/ItemCounter";
 
+interface ItemInCart {
+	productName: string;
+	quantity: number;
+}
+
+const itemsInCart: ItemInCart[] = [
+	{productName: "Nintendo Switch", quantity: 1},
+	{productName: "Mario Bros", quantity: 5},
+	{productName: "Switch Camera", quantity: 1},
+]
+
 export function FirstStepsApp() {
 	return (
 			<>
 					<h1>Carro de Compras 🛒</h1>
 					
-					<ItemCounter nombre="Nintendo Switch" cantidad={1}/>
-					<ItemCounter nombre="PS5" cantidad={1}/>
-					<ItemCounter nombre="Agua"/>
-					<ItemCounter nombre="Coca-Cola" cantidad={5}/>
+					{
+						itemsInCart.map(({productName, quantity}: ItemInCart) => (
+							<ItemCounter key={productName} nombre={productName} cantidad={quantity} />
+						))
+					}
 			</>
 	);
 }
