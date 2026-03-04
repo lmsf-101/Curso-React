1fd8dfc3a51810ff6f4ee657a73ebeebd9e11368
lmsf-101 <lmsf_academia_24@outlook.com>
2026-03-03 22:29:50 -0600

    58. Pruebas sobre eventos / acciones disparadas

--- 02-first-steps/src/shopping-cart/ItemCounter.test.tsx
+++ 02-first-steps/src/shopping-cart/ItemCounter.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import { describe, expect, test } from "vitest";
 import { ItemCounter } from "./ItemCounter";
 
@@ -24,12 +24,54 @@ describe('ItemCounter', () => {
     const quantity = 10;
     
     render(<ItemCounter nombre={name} cantidad={quantity} />);
-
+    
     // Verificar si la cantidad esta definida
     // OJO: En este caso, Vitest se encarga de transformar el valor
     // numero de "quantity" y lo transforma en texto   
     expect(screen.getByText(quantity)).toBeDefined()
+    
+  })
+  
+  test("Should increase count by 1 when button is pressed", () => {
+    
+    render(<ItemCounter nombre={'Test item'} cantidad={1} />);
 
+    const [buttonAdd] = screen.getAllByRole("button");
+
+    // Para disparar un evento, usa fireEvent
+    fireEvent.click(buttonAdd);
+
+    // Una vez disparado el evento, verificar si se realizaron los cambios
+    // En este caso, verificar que el contador haya cambiado a 2
+    expect(screen.getByText("2")).toBeDefined();
+
+    
+  })
+
+
+  test("Should decrease count by 1 when the minus button is pressed", () => {
+    // Quantity = 5
+    const quantity = 5;
+    
+    render(<ItemCounter nombre={'Test item'} cantidad={quantity} />);
+
+    const [, buttonSubtract] = screen.getAllByRole("button");
+
+    fireEvent.click(buttonSubtract);
+
+    expect(screen.getByText(quantity-1)).toBeDefined();
+    
+  })
+
+  test("Should NOT decrease count when the minus button is pressed, and quantity is 1", () => {
+    render(<ItemCounter nombre={'Test item'} cantidad={1} />);
+
+    const [, buttonSubtract] = screen.getAllByRole("button");
+
+    fireEvent.click(buttonSubtract);
+
+    expect(screen.getByText("1")).toBeDefined();
+    
   })
 
 })
