91a6e12c68f6566a026dac0a4b009eb69ea61f3a
lmsf-101 <lmsf_academia_24@outlook.com>
2026-03-07 15:29:02 -0600

    61. Intro a Componentes ficticios - Mock

--- 02-first-steps/src/FirstStepsApp.test.tsx
+++ 02-first-steps/src/FirstStepsApp.test.tsx
@@ -1,6 +1,15 @@
-import { render } from '@testing-library/react';
-import {describe, expect, test} from 'vitest';
+import { render, screen } from '@testing-library/react';
+import {describe, expect, test, vi} from 'vitest';
 import { FirstStepsApp } from './FirstStepsApp';
+
+// Realizar un "Mock" de ItemCounter
+// Simula un componente con el fin de hacer las pruebas mas faciles de manejar
+
+// Cuando alguien llame a este componente, se debe regresar lo siguiente
+// En vez del componente real
+vi.mock('./shopping-cart/ItemCounter', () => ({
+  ItemCounter: () => <div data-testid="ItemCounter" />,
+}));
 
 
 describe("FirstStepsApp", () => {
@@ -11,6 +20,22 @@ describe("FirstStepsApp", () => {
     const { container:con } = render(<FirstStepsApp />);
 
     expect(con).toMatchSnapshot();
+
+  })
+
+  // Prueba para demostrar un uso sencillo de los "Mocks": Mostrar
+  // la cantidad correcta de componentes
+  test("Should render the correct number of ItemCounter components", () => {
+
+    render(<FirstStepsApp />);
+
+    // Recuperar todos los componentes ItemCounter por el TestId:
+    const numItemCounter = screen.getAllByTestId("ItemCounter");
+
+    // Revisar si son tres componentes demostrados:
+    expect(numItemCounter.length).toBe(3);
+
+    screen.debug();
 
   })
 
