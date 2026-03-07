da0f062d86234e8836af9469bed6519747dd7ac3
lmsf-101 <lmsf_academia_24@outlook.com>
2026-03-07 16:45:17 -0600

    62. Uso de funciones 'espia' para evaluar numero de invocaciones a componente y args

--- 02-first-steps/src/FirstStepsApp.test.tsx
+++ 02-first-steps/src/FirstStepsApp.test.tsx
@@ -1,18 +1,32 @@
 import { render, screen } from '@testing-library/react';
-import {describe, expect, test, vi} from 'vitest';
+import {afterEach, describe, expect, test, vi} from 'vitest';
 import { FirstStepsApp } from './FirstStepsApp';
 
-// Realizar un "Mock" de ItemCounter
-// Simula un componente con el fin de hacer las pruebas mas faciles de manejar
 
-// Cuando alguien llame a este componente, se debe regresar lo siguiente
-// En vez del componente real
+// Aqui se genera una funcion "Mock-espia" que se puede encargar
+// de monitorear el estado de una funcion, sin modificar las instrucciones
+// dentro de ella
+
+// Esto es muy util para conocer cuantas veces fue llamado la funcion en 
+// el componente, que argumentos recibio, el retorno, etc.
+
+const MockItemCounter = vi.fn( (_props: unknown) => {
+  return (<div data-testid="ItemCounter"/>);
+});
+
+// Creacion de un "Mock" en base de la funcion anterior
 vi.mock('./shopping-cart/ItemCounter', () => ({
-  ItemCounter: () => <div data-testid="ItemCounter" />,
+  ItemCounter: (props: unknown) => MockItemCounter(props),
 }));
 
-
 describe("FirstStepsApp", () => {
+
+  // Dado que cada funcion invoca el componente 'Mock' / Funcion "espia"
+  // tres veces, es necesario "resetearlo" para cada prueba
+  // De manera que cada prueba sea independiente entre ellos
+  afterEach( () => {
+      vi.clearAllMocks();
+  });
 
   // Generar y verificar snapshot del componente y su estructura
   test("Should Match Snapshot", () => {
@@ -21,7 +35,7 @@ describe("FirstStepsApp", () => {
 
     expect(con).toMatchSnapshot();
 
-  })
+  });
 
   // Prueba para demostrar un uso sencillo de los "Mocks": Mostrar
   // la cantidad correcta de componentes
@@ -37,6 +51,39 @@ describe("FirstStepsApp", () => {
 
     screen.debug();
 
-  })
+  });
+
+  // NUEVA prueba para verificar con la funcion "espia" y componente "Mock"
+  // Para verificar el num de veces que se llamo la funcion / componente
+  // y la lista de argumentos enviados
+  test("Should render ItemCounter with correct props", () => {
+
+    render(<FirstStepsApp />)
+
+    // Verifica si fue llamado el componente de ItemCounter tres veces
+    // exactas
+    expect(MockItemCounter).toBeCalledTimes(3);
+
+    /*
+      Con la ayuda del componente Mock / funcion espia
+      Se verifica si se ha llamado el componente ItemCounter
+      con los siguientes argumentos listados:
+    */
+
+    expect(MockItemCounter).toHaveBeenCalledWith({
+      nombre: 'Nintendo Switch',
+      cantidad: 1,
+    });
+
+    expect(MockItemCounter).toHaveBeenCalledWith({
+      nombre: 'Mario Bros',
+      cantidad: 5,
+    });
+
+    expect(MockItemCounter).toHaveBeenCalledWith({
+      nombre: 'Switch Camera',
+      cantidad: 1,
+    });
+  });
 
 })
