e0e8ed9d69c5cded9d4642ab01f7f0f75d59af64
lmsf-101 <lmsf_academia_24@outlook.com>
2026-03-15 11:39:44 -0600

    72. Ejemplo de Manejo de estado y uso de funciones como argumentos

--- 03-gifs-app/src/GifsApp.tsx
+++ 03-gifs-app/src/GifsApp.tsx
@@ -3,13 +3,25 @@ import { CustomHeader } from './shared/components/CustomHeader'
 import { SearchBar } from './shared/components/SearchBar'
 import { PreviousSearches } from './gifs/components/PreviousSearches'
 import { GifList } from './gifs/components/GifList'
+import { useState } from 'react'
 
 // Idealmente, se genera un componente con el nombre de la misma aplicacion
 // Para uso de elementos globales (estilos, routers, etc)
 
 export const GifsApp = () => {
+  /* Uso de Hooks para administrar el historial de busquedas realizadas */
+  const [previousTerms, setPreviousTerms] = useState(['mario'])
+
+  // Ejemplo de como podemos enviar funciones entre componentes (padre y hijo)
+  // En este caso, mandar esta funcion hacia PreviousSearches:
+  const handleTermClicked = (term: string) => {
+    console.log( { term } )
+  };
+
   return (
     <>
+
+
       {/* Para mayor organizacion de nuestro proyecto, separamos cada
       componente de la aplicacion */}
 
@@ -24,7 +36,7 @@ export const GifsApp = () => {
 
       {/* Historial de busquedas */}
       {/* PreviousSearches */}
-      <PreviousSearches items={['Mario', 'Luigi', 'Goku']} />
+      <PreviousSearches items={previousTerms} onItemClick={handleTermClicked} />
 
       {/* GIFS */}
       {/* GifList: Props => gifs: Gif[] */}
--- 03-gifs-app/src/gifs/components/PreviousSearches.tsx
+++ 03-gifs-app/src/gifs/components/PreviousSearches.tsx
@@ -2,16 +2,20 @@ import type { FC } from "react"
 
 interface PreviousSearchesProps {
   items: string[]
+  // Agregar una funcion como argumento del componente:
+  onItemClick: (term: string) => void;
 }
 
-export const PreviousSearches: FC<PreviousSearchesProps> = ( {items} ) => {
+export const PreviousSearches: FC<PreviousSearchesProps> = ( {items, onItemClick} ) => {
   return (
     <div className='history-container'>
       <h2>Historial de Busquedas</h2>
       <ul className='history-list'>
         {
           items.map((item) => (
-          <li key={item}>{item}</li>
+          <li key={item}
+            onClick={() => onItemClick(item)}
+          >{item}</li>
         ))
         }
       </ul>
