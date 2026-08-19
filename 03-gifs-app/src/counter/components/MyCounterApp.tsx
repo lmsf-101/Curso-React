2f1dbc3e41d071349c67bf7b0ed75b2f3a09798a
lmsf-101 <lmsf_academia_24@outlook.com>
2026-08-19 15:49:50 -0600

    Codigo de ejemplo, para demo de Custom Hooks

---
+++ 03-gifs-app/src/counter/components/MyCounterApp.tsx
@@ -0,0 +1,38 @@
+import { useState } from "react";
+
+
+export const MyCounterApp = () => {
+
+  // Lógica de suma, resta, y reset, a resolver con el uso de Custom Hooks...
+  const [counter, setCounter] = useState(5);
+
+
+  const handleAdd = () => {
+    setCounter(counter + 1);
+  }
+
+  const handleSubtract = () => {
+    setCounter((prevState) => prevState - 1);
+  }
+
+  const handleReset = () => {
+    setCounter(5);
+  }
+
+  return (
+    <div style={{
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+    }}>
+      <h1> counter: { counter } </h1>
+
+      <div style={{display: 'flex', gap: '10px' }}>
+        <button onClick={handleAdd}>+1</button>
+        <button onClick={handleSubtract}>-1</button>
+        <button onClick={handleReset}>Reset</button>
+      </div>
+
+    </div>
+  )
+}
